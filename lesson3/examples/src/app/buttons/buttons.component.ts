02562c45d30e987cbfda84aeabe5d1284f46b5df
Matt <matthewearlywine@gmail.com>
2021-04-03 21:23:03 -0500

    NG lc101 projects Lesson 03 v.002

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -6,11 +6,27 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent implements OnInit {
-   buttonHeading: string = "Buttons"
+   buttonHeading: string = "Buttons";
+   theButtons: boolean[] = [false, false, false];
    inactive: boolean = false;
+   location: string = 'center';
 
    constructor() { }
 
    ngOnInit() { }
+
+   setButtons(){
+      for(let i = 0; i < this.theButtons.length; i++){
+          this.theButtons[i] = false;
+      }
+      return this.theButtons;
+   }
+
+   shiftLocation(oldLocation: string) {
+      while (this.location === oldLocation) {
+         this.location = ['left', 'right', 'center'][Math.floor(Math.random()*3)];
+      }
+      return this.location;
+   }
 
 }
